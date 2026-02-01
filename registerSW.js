ea78f3e73f67aff0b4dfec8544a0e102100894a1
Anthony Taylor <ant.taylor2@gmail.com>
2026-02-01 17:57:01 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sunken-cafe/sw.js', { scope: '/sunken-cafe/' })})}
